9eaaa86edb3e444ad14cd794314039b3904f9f97
MMostafa <mmostafa.ibra21@gmail.com>
2021-08-21 19:24:12 +0200

    Task : implement your your own Stack

---
+++ Task2_Stack/Task2_Stack/Source.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+
+char stack[100];
+int8_t sp = -1;
+void push(uint8_t u8_data)
+{
+	sp++;
+	stack[sp] = u8_data;
+}
+uint8_t pull()
+{
+	return stack[sp--];
+}
+uint8_t top()
+{
+	return stack[sp];
+}
+
+uint8_t* balancedParentheses(uint8_t* expression[])
+{
+	for (int i = 0; i < strlen(expression); i++)
+	{
+		if (expression[i] == '{' || expression[i] == '(')
+		{
+			push(expression[i]);
+		}
+		else if (expression[i] == '}' || expression[i] == ')')
+		{
+			if ((expression[i] == '}' && top() == '(') || (expression[i] == ')' && top() == '{') || (sp == -1))
+				return "Not Balanced";
+
+			pull();
+		}
+	}
+	if (sp != -1)
+		return "Not Balanced";
+	return "Balanced";
+}
+
+
+int main()
+{
+	char s[100];
+	gets(s);
+	printf("%s", balancedParentheses(s));
+}
